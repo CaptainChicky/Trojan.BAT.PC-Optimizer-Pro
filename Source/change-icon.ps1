916e32a07cd5a415dde49a7b9e372b26af4fa149
CaptainChicky <70708832+CaptainChicky@users.noreply.github.com>
2023-07-06 02:25:15 -0400

    d

--- Source/change-icon.ps1
+++
@@ -1,9 +0,0 @@
-$exePath = ".\PC Optimizer Pro.exe"
-$iconPath = ".\Assets\computer.ico"
-
-$shell = New-Object -ComObject Shell.Application
-$shortcut = $shell.CreateShortcut($exePath)
-$shortcut.IconLocation = $iconPath
-$shortcut.Save()
-
-$shell.Namespace((Split-Path -Parent $exePath)).ParseName((Split-Path -Leaf $exePath)).InvokeVerb("Properties")
